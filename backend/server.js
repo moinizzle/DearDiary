bbf7c9386602ea82d2e0b7b9e3a30fc51207197c
Moin Irfan <35012306+irfanmoi@users.noreply.github.com>
2020-07-15 01:28:18 -0400

    made changes

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,14 @@ app.get('/*', (req, res) => {
 }) 
 }
 
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../build'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+
 app.listen(process.env.PORT || 4444, () =>{
     console.log(`Server is running:`);})
     .on('error', err => {
