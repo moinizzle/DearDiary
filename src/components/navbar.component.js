185953797c38c01a654b922b0cea70b44d25a55d
Moin Irfan <35012306+irfanmoi@users.noreply.github.com>
2020-07-15 03:14:54 -0400

    fixed mobile

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 
 export default class NavigationBar extends Component {
