48327f80b745a5192a1a8a56cc31cf778a74d354
Moin Irfan <35012306+irfanmoi@users.noreply.github.com>
2020-07-14 22:47:25 -0400

    made changes

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use('/authors/', authorsRouter);
 // }
 
 app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../build'))
 })  
 
