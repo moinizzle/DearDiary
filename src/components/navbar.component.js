5c011b48113703f73b044cc0b0161103ef04f648
Moin Irfan <35012306+irfanmoi@users.noreply.github.com>
2020-07-16 03:05:29 -0400

    fixed mobile

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -10,7 +10,7 @@ export default class NavigationBar extends Component {
 
         return (
             <div>
-            <nav className="navbar navbar-expand-sm navbar-light bg-light" display= "flex">
+            <nav className="navbar navbar-expand-sm navbar-light bg-light" display= "flex" position="fixed">
             
                 <Link to="/" className="navbar-brand">
                 
